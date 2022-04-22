c54e304e0ac64d941442363ba9b0246eed1ebfe0
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-04-21 23:29:34 -0600

    models setup

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // foreignKey:'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // foreignKey:'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: ,
+  // foreignKey:'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: ,
+  // foreignKey:'product_tag_id'
+});
 
 module.exports = {
   Product,
